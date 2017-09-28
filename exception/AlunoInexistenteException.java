db9b0bafb995d806ccd597e4804bcb974b5a1758
HerculesGit <31658548+HerculesGit@users.noreply.github.com>
2017-09-27 23:18:53 -0300

    Add files via upload

---
+++ exception/AlunoInexistenteException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class AlunoInexistenteException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public AlunoInexistenteException(String s) {
+		super(s);
+	}
+}
---
+++ exception/ProfessorInexistenteException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class ProfessorInexistenteException extends Exception {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ProfessorInexistenteException(String s) {
+		super(s);
+	}
+}
