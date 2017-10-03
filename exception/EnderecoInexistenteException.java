44013cc8e9ed331290362416789ae19ddf4bbe07
HerculesGit <31658548+HerculesGit@users.noreply.github.com>
2017-10-03 16:19:34 -0300

    Add files via upload

---
+++ exception/EnderecoInexistenteException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class EnderecoInexistenteException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public EnderecoInexistenteException(String s) {
+		super(s);
+	}
+}
